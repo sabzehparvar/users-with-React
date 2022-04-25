0966856afd7bdf6e31f5a6272420ad238ae1f7d1
sabzehparvar <sabzehparvar.mojtaba@gmail.com>
2022-04-25 19:24:50 +0430

    new users updated

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -9,83 +9,111 @@ class Users extends Component {
   };
   async componentDidMount() {
     const response = await axios.get("https://reqres.in/api/users");
-    this.setState({ users: response.data.data, isLoading: false });
+    setTimeout(() => {
+      this.setState({ users: response.data.data, isLoading: false });
+    }, 2000);
   }
   render() {
     return (
       <>
-        <button className="btn btn-lg btn-primary" onClick={this.handleCreate}>
-          Create
-        </button>
-        <div className="row">
-          {this.state.isLoading ? (
-            <LoadingUsers />
-          ) : (
-            this.state.users.map((user) => {
-              return (
-                <div className="col-4 text-center p-5">
-                  <img
-                    src={user.avatar}
-                    style={{ borderRadius: "50%", width: "100px" }}
-                    alt=""
-                  />
-                  <h4>
-                    {user.first_name}{"  "}
-                    {user.last_name}
-                  </h4>
-                  <h5>{user.email}</h5>
-                  <div className="row">
-                    <div className="col-6 ">
-                      <button
-                        className=" btn btn-info btn-sm"
-                        onClick={() => {
-                          this.handleUpdate(user);
-                        }}
-                      >
-                        Update
-                      </button>
-                    </div>
+        <div className="container">
+          <p
+            className=" p-3 alert alert-info rounded-pill m-2"
+            style={{ "text-align": "center", direction: "rtl" }}
+          >
+            {" "}
+            این صفحه skeleton loading دارد و از قصد کاربران با تاخیر لود می شوند
+            تا بتوانید این لودینگ را ببینید
+          </p>
+          <div className="text-center p-4">
+            <button
+              className="btn btn-lg btn-outline-primary text-center"
+              onClick={this.handleCreate}
+            >
+              ایجاد کاربر جدید
+            </button>
+          </div>
+          <div className="row">
+            {this.state.isLoading ? (
+              <LoadingUsers />
+            ) : (
+              this.state.users.map((user) => {
+                return (
+                  <div className="col-lg-4 col-md-6 col-sm-12 text-center p-5">
+                    <img
+                      src={user.avatar}
+                      style={{ borderRadius: "50%", width: "100px" }}
+                      alt=""
+                    />
+                    <h4 className=" mt-2 text-warning">
+                      {user.first_name}
+                      {"  "}
+                      {user.last_name}
+                    </h4>
+                    <h5 className="text-dark">{user.email}</h5>
+                    <p className="text-success">{user.status}</p>
+                    <div className="row">
+                      <div className="col-6 ">
+                        <button
+                          className=" btn btn-outline-info btn-sm"
+                          onClick={() => {
+                            this.handleUpdate(user);
+                          }}
+                        >
+                          Update
+                        </button>
+                      </div>
 
-                    <div className="col-6 ">
-                      <button
-                        className=" btn btn-danger btn-sm"
-                        onClick={() => {
-                          this.handleDelete(user);
-                        }}
-                      >
-                        Delete
-                      </button>
+                      <div className="col-6 ">
+                        <button
+                          className=" btn btn-outline-danger btn-sm"
+                          onClick={() => {
+                            this.handleDelete(user);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })
-          )}
+                );
+              })
+            )}
+          </div>
+          <p
+            className=" p-3 alert alert-primary rounded-top m-2 mb-5"
+            style={{ "text-align": "center", direction: "rtl" }}
+          >
+            اطلاعات کاربران از این api گرفته می شود ==
+            ("https://reqres.in/api/users")
+          </p>
         </div>
       </>
     );
   }
 
   handleCreate = async () => {
-    const newUser = {
-      first_name: "ali",
-      last_name: "hoseyni",
-      email: "kjsdcbk@gmail.com",
-      avatar: "skjdckjds.jpg",
-    };
-    const response = await axios.post("https://reqres.in/api/users", newUser);
-    this.setState({ users: [...this.state.users, newUser] });
+    const newId = Math.ceil(Math.random() * 5) + 6;
+    const newUser = await axios.get(`https://reqres.in/api/users/${newId}`);
+    this.setState({ users: [...this.state.users, newUser.data.data] });
   };
   handleUpdate = async (user) => {
-    user.first_name = 'updated'
-    const response = await axios.put(`https://reqres.in/api/users ${user.id}`)
-    console.log(response);
-    const updatedUsers= [...this.state.users];
+    user.status = "updated";
+    const response = await axios.put(`https://reqres.in/api/users ${user.id}`);
+    const updatedUsers = [...this.state.users];
     const index = updatedUsers.indexOf(user);
-    updatedUsers[index]= {...user};
-    this.setState({users:updatedUsers})
+    updatedUsers[index] = { ...user };
+    this.setState({ users: updatedUsers });
   };
-  handleDelete = (user) => {};
+  handleDelete = (user) => {
+    const id = user.id;
+    const users = [...this.state.users];
+
+    const newUsers = users.filter((user) => {
+      return user.id !== id;
+    });
+    this.setState({ users: newUsers });
+  };
 }
 
 export default Users;
